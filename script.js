22f31e372bb9108920ead2046b523d316cf09534
Ayush Singh <142167916+ayush-singh09@users.noreply.github.com>
2023-11-08 18:50:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,58 @@
+var menubtn = document.querySelector(".menu");
+var menuScreen = document.querySelector(".menu-screen");
+var close = document.querySelector(".ri-close-fill");
+var curser = document.querySelector(".cur");
+var pic = document.querySelector(".pic");
+var extra = document.querySelector(".extra");
+
+
+
 const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+window.addEventListener("mousemove",function(dets){
+
+    gsap.to(".cur",{
+        top : dets.clientY,
+        left : dets.clientX
+    })
+})
+
+
+
+function picMouseEvents(){
+    pic.addEventListener("mousemove",function(dets){
+    
+        gsap.to(".extra",{
+            top:dets.clientY-pic.getBoundingClientRect().top,
+            left:dets.clientX-pic.getBoundingClientRect().left
+        })
+    })
+
+    pic.addEventListener("mouseenter",function(){
+        gsap.to('.extra',{
+            scale:1,
+            translateX:-50 + "%",
+            translateY:-50 + "%"
+        })
+        gsap.to('.cur',{
+            scale:0
+        })
+
+    })
+    pic.addEventListener("mouseleave",function(){
+        gsap.to('.extra',{
+            scale:0
+        })
+        gsap.to('.cur',{
+            scale:1
+        })
+        
+    })
+}
+picMouseEvents();
 
 
 var cap = document.querySelector(".cap");
@@ -16,9 +67,6 @@ function changeText() {
 
 setInterval(changeText, 1000); // Change the text every 1 second
 
-var menubtn = document.querySelector(".menu");
-var menuScreen = document.querySelector(".menu-screen")
-var close = document.querySelector(".ri-close-fill")
 
 menubtn.addEventListener('click',function(){
     menuScreen.style.display = "flex"
@@ -34,13 +82,12 @@ menubtn.addEventListener('click',function(){
 
 close.addEventListener('click',function(){
     gsap.to(".menu-screen",{
-        top:-80 + "%"
+        top:-100 + "%"
     })
 
     setTimeout(function(){
         menuScreen.style.display = "none"
-    },100)
-    // menuScreen.style.top = "-100%"
+    },500)
 });
 
-// menuScreen.style.height = window.innerHeight + 'px';
+menuScreen.style.height = window.innerHeight + 'px';
